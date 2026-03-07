41ac0453c048e1842407e42aee3a715b73df62cd
unknown <luccazappala@gmail.com>
2026-03-06 21:04:11 -0300

    feat: add pagina de login e styles

---
+++ gerenciador-eventos-web/src/pages/Login.styles.ts
@@ -0,0 +1,129 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    background-color: #003B49;
+    font-family: 'Inter', sans-serif;
+`;
+
+export const WelcomeSection = styled.section`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 40px;
+    text-align: center;
+
+    h1 {
+        font-size: 2.5rem;
+        color: #FFF;
+        margin-bottom: 10px;
+        max-width: 400px;    
+    }
+
+    p {
+        color: #2B94A7;
+        font-weight: bold;
+        margin-bottom: 30px;
+    }
+`;
+
+export const FormSection = styled.section`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 40px;
+`;
+
+export const LogoNeki = styled.img`
+    width: 200px;
+    margin-bottom: 40px;
+    `;
+
+ export const Mascote = styled.img`
+    width: 350px;
+`;   
+
+export const FormCard = styled.form`
+    width: 100%;
+    max-width: 400px;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+
+    h2{
+        color: #FFF;
+        font-size: 1.8rem;
+        margin-bottom: 10px;
+    }
+`;
+
+export const Input = styled.input`
+    width: 100%;
+    padding: 16px;
+    border-radius: 8px;
+    border: none;
+    background-color: #2B94A7;
+    color: #FFF;
+    font-size: 1rem;
+
+    &::placeholder {
+        color: rgba(255, 255, 255, 0.7)
+    }
+`;
+
+export const CheckBoxContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    color: #FFF;
+    font-size: 0.9rem;
+    margin-top: 5px;
+
+    input{
+        cursor: pointer;
+    }
+`;
+
+export const Button = styled.button` 
+    width: 100%;
+    padding: 16px;
+    border-radius: 8px;
+    border: none;
+    background-color: #FFF;
+    color: #003B49;
+    font-weight: bold;
+    font-size: 1rem;
+    cursor: pointer;
+    margin-top: 10px;
+    transition: filter 0.2s;
+
+    &:hover {
+        filter: brightness(0.9);
+    }
+`;
+
+export const SecondaryButton = styled.button`{
+    background: none;
+    border: 1px solid #FFF;
+    color: #FFF;
+    padding: 12px;
+    border-radius: 8px;
+    cursor: pointer;
+    margin-top: 10px;
+    font-weight: bold;
+
+    &:hover{
+        background-color: rgba(255, 255, 255, 0.1)
+    }
+`;
+
+
+
+
+
---
+++ gerenciador-eventos-web/src/pages/Login.tsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../services/api'
+
+import * as S from './Login.styles';
+import mascoteImg from '../assets/mascoteNeki.png'
+import logoImg from '../assets/logotipoNekiBranca.png'
+
+
+
+const Login: React.FC = () => {
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+    const[rememberMe, setRememberMe] = useState(false);
+
+    const Navigate = useNavigate(); 
+
+    useEffect(() => {
+        const savedEmail = localStorage.getItem('@NekiEvents: email');
+        const savedPass = localStorage.getItem('@NekiEvents: password');
+        if(savedEmail && savedPass){
+            setEmail(savedEmail);
+            setPassword(savedPass);
+            setRememberMe(true);
+        }
+    }, []);
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try{
+            const response = await api.post('/administradores/login', {
+                email: email,
+                senha: password
+            });
+
+            const { token } = response.data;
+
+            localStorage.setItem('@NekiEvents:token', token);
+
+            if(rememberMe) {
+                localStorage.setItem('@NekiEvents:email', email);
+                localStorage.setItem('@NekiEvents:password', password);
+            } else {
+                localStorage.removeItem('@NekiEvents:email');
+                localStorage.removeItem('@NekiEvents:password');
+            }
+
+            Navigate('/home');
+        } catch (error) {
+            console.error(error);
+            alert('Falha no login. Verifique email e senha.')
+        }
+    };
+
+    return(
+
+        <S.Container>
+            <S.WelcomeSection>
+                <h1>BEM VINDO(A)! CADASTRE SEUS EVENTOS NO NEKI EVENTOS!</h1>
+                <p>FAÇA LOGIN OU CADASTRE-SE</p>
+                <S.Mascote src={mascoteImg} alt="Mascote Neki"/>
+            </S.WelcomeSection>
+
+            <S.FormSection>
+                <S.LogoNeki src={logoImg} alt="Logo Neki" />
+
+                <S.FormCard onSubmit={handleLogin}>
+                    <h2>Login</h2>
+
+                    <S.Input
+                    type="email"
+                    placeholder="E-mail"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    />
+
+                    <S.Input
+                    type="password"
+                    placeholder="Senha"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    />
+
+                    <S.CheckBoxContainer>
+                        <input
+                        type="checkbox"
+                        id="remember"
+                        checked={rememberMe}
+                        onChange={(e) => setRememberMe(e.target.checked)}
+                        />
+                        <label htmlFor="remember">Gravar Senha</label>
+                    </S.CheckBoxContainer>
+
+                    <S.Button type="submit">ENTRAR</S.Button>
+
+                    <S.SecondaryButton type="button" onClick={() => Navigate('/cadastro')}>
+                        CADASTRAR-SE
+                    </S.SecondaryButton>
+
+
+                </S.FormCard>
+            </S.FormSection>
+        </S.Container>
+
+    );
+
+};
+
+export default Login;
